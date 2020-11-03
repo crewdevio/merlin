119ff883193892028c6fae0bd8212ec42eea0d5c
Erick Sosa Garcia <ericksosagarcias@gmail.com>
2020-11-03 13:13:42 -0400

    feat: data type improvements

--- src/types.ts
+++ src/types.ts
@@ -1,94 +1,90 @@
-import type { bench } from "../imports/testing.ts";
-
-export interface testConfig<T extends unknown | any> {
+interface Fields {
+  /** receives a boolean to ignore the test in case the value is true */
   ignore?: boolean;
-  expect(): Promise<T> | T;
-  toBe(): Promise<T> | T;
-  strict?: boolean;
+  /** receives a string with the message to display in case the test fails */
   message?: string;
+  /** receives a boolean, closes all the operations that never end */
   Ops?: boolean;
+  /** receives a boolean, terminates all asynchronous processes that interact with the system */
   Resources?: boolean;
+  /** receives a boolean, only tests that have only in true will be executed, the rest will not run */
   only?: boolean;
+  /** execute any action before run test */
+  before?: () => void | undefined | Promise<void | undefined>;
 }
 
-export interface Test<T> extends testConfig<T> {
+interface Strict {
+  /** receives a boolean, it does a strict comparison of the values */
+  strict?: boolean;
+}
+
+export interface Is<T extends unknown> extends Fields {
+  /** returns the data expected to be of that type */
+  value(): Promise<T> | T;
+}
+
+export interface Asserts<T extends unknown | any> extends Fields {
+  /** this function returns the data and then tests with its matchmaker */
+  expect(): Promise<T> | T;
+  /** this function returns the data that we hope is correct */
+  toBe(): Promise<T> | T;
+}
+
+export interface AssertsStrict<T extends unknown | any>
+  extends Fields,
+    Strict,
+    Asserts<T> {}
+
+export interface Test<T> extends Fields, AssertsStrict<T> {
+  /** add a description to the test */
   label: string;
 }
 
-export interface Config<T extends unknown> {
-  ignore?: boolean;
-  value(): Promise<T> | T;
-  message?: string;
-  Ops?: boolean;
-  Resources?: boolean;
-  only?: boolean;
-}
-
-export interface Fetch_equal {
+export interface Fetch_equal extends Fields {
   url: string;
   config?: RequestInit;
   type: "text" | "json";
+  /** this function returns the data that we hope is correct */
   toBe(): Promise<any> | any;
-  message?: string;
-  ignore?: boolean;
-  Ops?: boolean;
-  Resources?: boolean;
-  only?: boolean;
 }
 
-export interface ArrayContains<T extends unknown> {
-  ignore?: boolean;
+export interface ArrayContains<T extends unknown> extends Fields {
+  /** returns the data expected to be of that type */
   value(): Promise<T[]> | T[];
   Contains(): Promise<T[]> | T[];
-  message?: string;
-  Ops?: boolean;
-  Resources?: boolean;
-  only?: boolean;
 }
 
-export interface StringIncludes {
-  ignore?: boolean;
+export interface StringIncludes extends Fields {
+  /** returns the data expected to be of that type */
   value(): Promise<string> | string;
   Contains(): Promise<string> | string;
-  message?: string;
-  Ops?: boolean;
-  Resources?: boolean;
-  only?: boolean;
 }
 
-export interface NotEqual {
+export interface NotEqual extends Fields {
+  /** this function returns the data and then tests with its matchmaker */
   expect(): Promise<any> | any;
+  /** this function returns the data that we hope it is incorrect */
   notBe(): Promise<any> | any;
-  ignore?: boolean;
-  message?: string;
-  Ops?: boolean;
-  Resources?: boolean;
-  only?: boolean;
 }
 
-export interface Length {
-  ignore?: boolean;
+export interface Length extends Fields {
+  /** this function returns the data and then tests with its matchmaker */
   expect(): Promise<ArrayLike<any>> | ArrayLike<any> | string | Promise<string>;
+  /** this function returns the data that we hope is correct */
   toBe(): Promise<ArrayLike<any>> | ArrayLike<any> | string | Promise<string>;
-  message?: string;
-  Ops?: boolean;
-  Resources?: boolean;
-  only?: boolean;
 }
 
-export interface throws {
-  throws(): void | Promise<void>;
-  ignore?: boolean;
-  message?: string;
-  Ops?: boolean;
-  Resources?: boolean;
-  only?: boolean;
+export interface throws extends Fields {
+  throws(): undefined | void | Promise<void | undefined>;
   ErrorClass?: any;
 }
 
 export interface Bench {
+  /** benchmark name */
   name: string;
+  /**  number of times to repeat the benchmark */
   steps?: number;
+  /** function that contains the code */
   fn: Function;
 }
 
@@ -102,8 +98,6 @@ export interface EmitConfig {
   description?: string;
   json?: boolean;
 }
-
-export type BenchResult = bench.BenchmarkResult;
 
 export type Tests<T> = Array<Test<T>>;
 
